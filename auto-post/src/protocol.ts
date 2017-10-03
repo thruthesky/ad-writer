43c6824bf190d5379a0be27cd55e90551320d640
gemlorenz10 <gemlorenz10@gmail.com>
2017-10-03 21:52:39 +0800

    put new line for each <p> element's content.

--- auto-post/src/protocol.ts
+++ auto-post/src/protocol.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 export let protocolName;
 
 export function set(pid) {
@@ -18,10 +19,26 @@ export function error(code, msg = ''){
     process.exit(1);
 }
 
-export function removeTags( post : string, cheerio = require('cheerio') ) {
+export function removeTags( post : string, $ = require('cheerio') ) {
+    let arr = [];
+    let $html = $.load(post)('p');
+    $html.each( function( i, element ){
+        arr[i] = $(this).text();
+    } );
 
-    let content = cheerio.load(post)('html').text();
-    let imageUrl = cheerio.load(post)('img').attr('src');
+    let siteUrl = $.load(post)('a').attr('href');
 
-    return content + '\r\n' +imageUrl;
+    return arr.join('\r\n') + siteUrl;
+    // return  contents + '\r\n' + siteUrl;
+//    console.log( contents + '\r\n' + siteUrl );
 }
+
+
+// removeTags( 
+//     `
+//     <h1>This is title</h1>
+//     <p>
+//     This is contents with link inside. <a href="https://www.sonub.com">click me</a>
+//     </p>
+//     `
+//  )
