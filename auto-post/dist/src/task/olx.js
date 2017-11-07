9d4ea55cb93308780ca1fa1ead1a6a3112d0fe95
JaeHo Song <thruthesky@gmail.com>
2017-11-07 13:56:50 +0800

    up

--- auto-post/dist/src/task/olx.js
+++ auto-post/dist/src/task/olx.js
@@ -18,8 +18,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -72,10 +72,10 @@ var Olx = (function (_super) {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4, this.login()];
+                    case 0: return [4 /*yield*/, this.login()];
                     case 1:
                         _a.sent();
-                        return [2];
+                        return [2 /*return*/];
                 }
             });
         });
@@ -85,10 +85,10 @@ var Olx = (function (_super) {
             var _this = this;
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4, this.get(this.olxUrl).catch(function (e) { return _this.captureError('Error opening the page.', _this.olxUrl); })];
+                    case 0: return [4 /*yield*/, this.get(this.olxUrl).catch(function (e) { return _this.captureError('Error opening the page.', _this.olxUrl); })];
                     case 1:
                         _a.sent();
-                        return [2];
+                        return [2 /*return*/];
                 }
             });
         });
@@ -102,11 +102,11 @@ var Olx = (function (_super) {
                     case 0:
                         if (!fs.existsSync(filePath))
                             fs.mkdirSync(filePath);
-                        return [4, this.screenshot(path.join(filePath, fileName))];
+                        return [4 /*yield*/, this.screenshot(path.join(filePath, fileName))];
                     case 1:
                         _a.sent();
                         protocol.fail(message + 'Check screenshot at :' + path.join(filePath, fileName));
-                        return [2];
+                        return [2 /*return*/];
                 }
             });
         });
